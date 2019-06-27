6bfc2361f8ec2e851d639e1c7b03263f063f7195
bigkang <bigkang>
2019-06-27 16:09:53 +0800

    初始化

---
+++ boot-hanlp/src/main/java/com/kang/boot/hanlp/BootHanlpApplication.java
@@ -0,0 +1,11 @@
+package com.kang.boot.hanlp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootHanlpApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(BootHanlpApplication.class,args);
+    }
+}
